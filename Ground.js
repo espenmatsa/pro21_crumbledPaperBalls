563777fa63b509db627b244bc4f1229ce5ab0806
espenmatsa <87954862+espenmatsa@users.noreply.github.com>
2022-06-16 20:25:48 -0400

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,29 @@
+class Ground {
+    constructor(x, y, width, height){
+        this.x= x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        var options = {
+            isStatic: true
+        };
+
+        this.body = Bodies.rectangle(x, y, width, height,options);
+        World.add(world, this.body);
+    }
+
+
+    display(){
+        var pos = this.body.position;
+        push();
+        rectMode(CENTER);
+        stroke("red");
+        fill("green");
+        rect(pos.x, pos.y, this.width, this.height);
+        pop();
+    }
+}
+
+
+
